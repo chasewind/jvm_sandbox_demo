bfbc350f39e5ddb7b3424ed2def62c4923581916
yudongwei <dongwei.yu@vevor.onaliyun.com>
2025-04-01 19:38:27 +0800

    print class file to show more details

--- jvm_sandbox_bytebuddy_agent/src/main/java/com/deer/agent/AgentMainForByteBuddy.java
+++ jvm_sandbox_bytebuddy_agent/src/main/java/com/deer/agent/AgentMainForByteBuddy.java
@@ -1,5 +1,6 @@
 package com.deer.agent;
 
+import net.bytebuddy.agent.ByteBuddyAgent;
 import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.description.type.TypeDescription;
@@ -16,13 +17,22 @@ import java.lang.instrument.Instrumentation;
 
 public class AgentMainForByteBuddy {
     public static void agentmain(String agentArgs, Instrumentation inst){
+        // 指定输出目录
+        String outputDir = "./output/";
+        File dir = new File(outputDir);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
         AgentBuilder.Transformer transformer = (builder, typeDescription, classLoader, javaModule) -> {
 //            return builder.method(ElementMatchers.any()).intercept(MethodDelegation.to(MethodCostTime.class));
-//            return builder.method(ElementMatchers.any()).intercept(Advice.to(MethodInterceptor.class));
 
             if (classLoader != null && typeDescription.getName().startsWith("com.deer.base.service")) {
+                DynamicType.Unloaded<?> dynamicType = builder.method(ElementMatchers.any()).intercept(Advice.to(MethodInterceptor.class)).make();
+                // 将字节码写入文件
+                writeClassToFile(dynamicType, dir);
                 return builder.method(ElementMatchers.any()).intercept(Advice.to(MethodInterceptor.class));
             }
+
             return builder; // 不进行转换
         };
         AgentBuilder.Listener listener = new AgentBuilder.Listener() {
@@ -53,27 +63,7 @@ public class AgentMainForByteBuddy {
 //                System.out.println("onComplete: " + s);
             }
         };
-        // 定义一个ClassFileTransformer，用于将修改后的类文件写入指定目录
-        ClassFileTransformer classFileTransformer = (loader, className, classBeingRedefined, protectionDomain, classfileBuffer) -> {
-            // 将类文件写入指定目录
-            String outputDirectory = "target/transformed-classes";
-            File outputDir = new File(outputDirectory);
-            if (!outputDir.exists()) {
-                outputDir.mkdirs();
-            }
 
-            // 构造目标文件路径
-            String targetClassName = className.replace('/', File.separatorChar) + ".class";
-            File classFile = new File(outputDir, targetClassName);
-
-            try (FileOutputStream fos = new FileOutputStream(classFile)) {
-                fos.write(classfileBuffer);
-                System.out.println("Modified class file written to: " + classFile.getAbsolutePath());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return classfileBuffer;
-        };
         new AgentBuilder
                 .Default()
                 .type(ElementMatchers.nameStartsWith("com.deer.base.service"))
@@ -83,5 +73,21 @@ public class AgentMainForByteBuddy {
                 .installOn(inst);
         // 注册ClassFileTransformer
 //        inst.addTransformer(classFileTransformer);
+    }
+    private static void writeClassToFile(DynamicType.Unloaded<?> dynamicType, File dir) {
+        String className = dynamicType.getTypeDescription().getName();
+        String fileName = className.replace('.', '/') + ".class";
+        File outputFile = new File(dir, fileName);
+        // 创建父目录
+        File parentDir = outputFile.getParentFile();
+        if (!parentDir.exists()) {
+            parentDir.mkdirs(); // 确保父目录存在
+        }
+        try (FileOutputStream fos = new FileOutputStream(outputFile)) {
+            fos.write(dynamicType.getBytes());
+            System.out.println("Class written to: " + outputFile.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
